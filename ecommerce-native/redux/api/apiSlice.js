d7372a9b13858ef546670873a35440f48fd07d1f
User <user@MMSs-MacBook-Air.local>
2024-09-06 21:23:48 +0630

    change url

--- ecommerce-native/redux/api/apiSlice.js
+++ ecommerce-native/redux/api/apiSlice.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://3.88.234.95",
+  baseUrl: "http://3.1.203.31",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token;
     if (token) {
