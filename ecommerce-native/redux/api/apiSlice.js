db4f7a9fcc20c7df1996de8b4f0c758ea41b32d3
User <user@MMSs-MacBook-Air.local>
2024-09-08 20:20:18 +0630

    localhost

--- ecommerce-native/redux/api/apiSlice.js
+++ ecommerce-native/redux/api/apiSlice.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://3.1.203.31",
+  baseUrl: "http://localhost",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token;
     if (token) {
